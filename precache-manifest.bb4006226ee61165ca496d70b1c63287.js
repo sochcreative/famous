a23876ed59723712f37a9b16483079d8376916a2
pradeep yadav <pradeep.yadav@beganto.com>
2020-10-12 23:07:18 +0530

    Updates

---
+++ precache-manifest.bb4006226ee61165ca496d70b1c63287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5aa180a1a57546245ba1e32647562c",
+    "url": "/famous/index.html"
+  },
+  {
+    "revision": "c59cac5d82e62d4905a2",
+    "url": "/famous/static/css/main.aa31e8c0.chunk.css"
+  },
+  {
+    "revision": "de49a153782957f32bbc",
+    "url": "/famous/static/js/2.e605c4d9.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/famous/static/js/2.e605c4d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59cac5d82e62d4905a2",
+    "url": "/famous/static/js/main.f1b9f263.chunk.js"
+  },
+  {
+    "revision": "bbb63a233386c649cf44",
+    "url": "/famous/static/js/runtime-main.b5e05a33.js"
+  }
+]);
